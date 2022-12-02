f6ec700dede85893c1069c9601b17bec5e978564
LilNoahChan <noahchan305@gmail.com>
2022-12-02 13:15:03 -0500

    12/2

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,9 +1,9 @@
-import "../styles/style.css"
-// export { menu } from "./menu";
-
-document.querySelector('#app').innerHTML = `
-    <h1>Hello???</h1>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-    `;
-
-
+document.querySelector(".btn").addEventListener("click", function () {
+    if (document.body.classList.contains("cool")) {
+      document.body.classList.add("warm");
+      document.body.classList.remove("cool");
+    } else {
+      document.body.classList.add("cool");
+      document.body.classList.remove("warm");
+    }
+  });
