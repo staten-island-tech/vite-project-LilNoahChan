932c6eb9bfcf24726c57e9bc41d7d29a2c8be2f6
LilNoahChan <noahchan305@gmail.com>
2022-12-13 13:13:54 -0500

    12/13

--- vite-project/js/index.js
+++ vite-project/js/index.js
@@ -1,13 +1,3 @@
-document.querySelector(".btn").addEventListener("click", function () {
-  if (document.body.classList.contains("light")) {
-    document.body.classList.add("dark");
-    document.body.classList.remove("light");
-  } else {
-    document.body.classList.add("light");
-    document.body.classList.remove("dark");
-  }
-});
-
 const DOMselectors ={
 button: document.getElementById("b1"),
 button2: document.getElementById("b2"),
@@ -16,3 +6,15 @@ button4: document.getElementById("b4"),
 button5: document.getElementById("b5"),
 box: document.getElementById("container"),
 };
+
+export { DOMselectors };
+
+DOMselectors.button.addEventListener("click", function () {
+  if (document.body.classList.contains("light")) {
+    document.body.classList.add("dark");
+    document.body.classList.remove("light");
+  } else {
+    document.body.classList.add("light");
+    document.body.classList.remove("dark");
+  }
+});
--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,18 +1,19 @@
-import "./styles/style.css";
+import "../styles/style.css";
 import { cars } from "./cars";
 console.log(cars);
+import { DOMselectors } from "./index";
 
 const commands = {
     createcards: function(){
-        carsData.forEach((cars) => {
-            DOMselectors.box.insertAdjacentHTML(
+        cars.forEach(cars => {DOMselectors.box.insertAdjacentHTML(
               "beforeend",
               `
-                <div id="productCard">
+                <div id="container">
                 <h2>${cars.name}</h2>
                 <p><img src=${cars.imgURL}></p>
                 <p>Year: ${cars.year}</p>
-                <p>Price: ${cars.cost}</p>
+                <p>Cost: ${cars.cost}</p>
+                </div>
               `
             );
           });
